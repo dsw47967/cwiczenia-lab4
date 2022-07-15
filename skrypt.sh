343593b26938717da0d8501ca2dd6c27f17e7297
dsw47967 <dsw47967@student.dsw.edu.pl>
2022-07-15 18:53:06 +0200

    dodanie flagi log

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 case $1 in
- 
-
+  
   --date)
     echo "$(date +'%m/%d/%Y')"
     ;;
 
- 
+  --logs)
+    if [ -z "$2" ]
+      then
+      x=100
+    else
+      x=$2
+    fi
+    for i in {1..$x}
+    do
+      echo "log$i.txt $0 $(date +'%m/%d/%Y')" >> "log$i.txt"
+    done
+    ;;
+
 esac
