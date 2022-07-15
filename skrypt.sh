329bafb5d7c49b7255982f3103c76138f225948f
dsw47967 <dsw47967@student.dsw.edu.pl>
2022-07-15 19:33:51 +0200

    przerobienie skryptu na to z polecenia 2

--- skrypt.sh
+++ skrypt.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 
 case $1 in
-  --help)
+  --help | -h)
     echo 
     "
-    --help  twyswietla pomoc
-    --date  wyswietla aktualna date
-    --logs  tworzy 100 plikow log(x).txt
-    --logs <liczba>  tworzy X plikow log(x).txt
+    --help, -h  twyswietla pomoc
+    --date, -d  wyswietla aktualna date
+    --logs, -l  tworzy 100 plikow log(x).txt
+    --logs, -l <liczba>  tworzy X plikow log(x).txt
+    --error, -e <liczba> tworzy X plików error(x).txt
     "
     ;;
 
-  --date)
+  --date | -d)
     echo "$(date +'%m/%d/%Y')"
     ;;
 
-  --logs)
+  --logs | -l)
     if [ -z "$2" ]
       then
       x=100
     else
       x=$2
     fi
+    rm -rf "log$x"
+    mkdir "log$x"
     for i in {1..$x}
     do
-      echo "log$i.txt $0 $(date +'%m/%d/%Y')" >> "log$i.txt"
+      echo "log$i.txt $0 $(date +'%m/%d/%Y')" >> "log$x/log$i.txt"
+    done
+    ;;
+
+  --error | -e)
+    if [ -z "$2" ]
+      then
+      x=100
+    else
+      x=$2
+    fi
+    rm -rf "error$x"
+    mkdir "error$x"
+    for i in {1..$x}
+    do
+      echo "error$i.txt $0 $(date +'%m/%d/%Y')" >> "error$x/error$i.txt"
     done
     ;;
 
